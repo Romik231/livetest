394483c472350165dc181ff673b8e33774ab0974
Romik231 <Romik231@yandex.ru>
2019-10-23 23:59:46 +0300

    Начал разбираться с jquery

--- public/scripts/send_users.js
+++ public/scripts/send_users.js
@@ -1,18 +1,55 @@
 $(document).ready(function () {
-    $('#btn-send').click(function () {
-       sendAjaxForm('form_add', '../models/Users.php');
+    $("#btn-send").click(function () {
+        $.ajax({
+            type: 'POST',
+            url: 'http://livetest/public/index.php?c=page&act=users',
+            dataType: 'html',
+            data: $('#form_user'),
+            success: function (response) {
 
+
+
+                console.log(response);
+        }
+        });
+
+
+        // $.post(
+        //     'http://livetest/public/index.php?c=page&act=users',
+        //     $("#form_add").serialize(),
+        //
+        //     function (msg) {
+        //         $("#result_form").html(msg);
+        //         // console.log(msg);
+        //     }
+        // );
+        // alert('форма успешно отправлена');
+        // return false;
     });
 });
 
-function sendAjaxForm(form_add, url) {
-    $.ajax({
-        url: url,
-        type: 'POST',
-        dataType: 'hmtl',
-        data:$('#'+form_add).serialize(),
-        success: function (response) {
-            $('#result-form'.html('данные отпралены'));
-        }
-    });
-}
+
+
+
+//     $('#btn-send').click(
+//         function () {
+//             sendAjaxForm('#form_add', );
+//                 return false;
+//     });
+// });
+//
+// function sendAjaxForm(form_add) {
+//     $.ajax({
+//         url: 'http://livetest/public/index.php?c=page&act=users',
+//         type: 'POST',
+//         dataType: 'hmtl',
+//         success: function (response) {
+//             result = $.parseJSON(response)
+//             $('#result-form').html('имя'+result.name);
+//         },
+//         error: function (response) {
+//             $('#result-form').html('Ошибка');
+//
+//         }
+//     });
+// }
--- views/users.php
+++ views/users.php
@@ -9,7 +9,7 @@
 <?//=var_dump($city);?>
 
 <h3>Добавить пользователя</h3>
-<form action="" method="post" class="form" id="form_add">
+<form method="post" id="form_user" class="form" id="form_add">
     <label for="name">Имя</label>
     <input type="text" name="name" id="name" required="required">
     <label for="age">Возраст</label>
@@ -23,9 +23,10 @@
         <? endforeach;?>
     </select>
 
-    <button type="submit" id="btn-send" class="send">Добавить</button>
+    <button type="button" id="btn-send" class="send">Добавить</button>
 </form>
 
+<div id="result_form"></div>
 
 
 <div class="users">
@@ -36,7 +37,7 @@
             <td>Город</td>
         </tr>
         <?php foreach ($users as $key => $val):?>
-        <?=var_dump($val)?>
+<!--        --><?//=var_dump($_POST);?>
         <tr>
             <td><?=$val['name'];?></td>
             <td><?=$val['age'];?></td>
