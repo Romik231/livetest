0ea8dc0761254da49a0543d452da6b227a855773
Romik231 <Romik231@yandex.ru>
2019-10-28 01:31:57 +0300

    доработал немного для вывода городов

--- models/model.php
+++
@@ -1,1 +0,0 @@
-<?phpnamespace models;class model{    public function text_get()    {        return file_get_contents('../data/text.txt');    }    public function text_set($text)    {        file_put_contents('../data/text.txt', $text);    }}
